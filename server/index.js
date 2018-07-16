0f9cb7ebc809a5aee07fcccaaae83bfd6acd4c60
Cody Falzone <cfalz002@ucr.edu>
2018-07-16 13:37:44 -0700

    update greeting

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({MERN: 'STACK'});
+    res.send({Hi: 'Robert'});
 });
 
 const PORT = process.env.PORT || 5000;
