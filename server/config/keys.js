a3236cc7ceef19c0146e57b45b08ae5d83096a93
Cody Falzone <cfalz002@ucr.edu>
2018-07-18 10:37:13 -0700

    finished auth flow

---
+++ server/config/keys.js
@@ -0,0 +1,7 @@
+if(process.env.NODE_ENV === 'production') {
+    //we are in production - return the production set of keys    
+    module.exports = require('./prod');
+} else {
+    //we are in development - return the development set of keys
+    module.exports = require('./dev');
+}
